0be686390bdb3c8bb3e857f1ee067bf78099393f
Mon Maldonado <monterdi@gmail.com>
2017-04-07 06:20:53 +0200

    portrait CollectionView with 14 items

--- Gridtest/ViewController.swift
+++ Gridtest/ViewController.swift
@@ -9,17 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var collectionView: UICollectionView!
+    var cellId = "newCell"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 20,
+                                           left: 10,
+                                           bottom: 10,
+                                           right: 10)
+        layout.itemSize = CGSize(width: 90,
+                                 height: 120)
+        
+        collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView.backgroundColor = UIColor.white
+        self.view.addSubview(collectionView)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    //UICollectionViewDelegate es redundante ya que forma parte de UICollectionViewDelegateFlowLayout -> pero quiero dejarlo patente
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 14
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = UIColor.orange
+        return cell
+    }
+    
+}
